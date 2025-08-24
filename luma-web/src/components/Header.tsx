4e69fee0e2d516de015c477b34fec997a7fa634e
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2025-08-24 21:56:35 +0400

    feature/i8n

--- luma-web/src/components/Header.tsx
+++ luma-web/src/components/Header.tsx
@@ -1,22 +1,27 @@
-import Image from "next/image";
-import React from "react";
+import Image from 'next/image';
+import React from 'react';
+import { useTranslations } from 'next-intl';
+import LangTransitionH2 from '@/lib/LangTransitionH2';
+import LocaleSwitcher from './LocaleSwitcher';
 
 export default function Header() {
   return (
-    <div className="flex justify-center flex-col">
-      <nav className="flex justify-between items-center">
-        <Image src="/luma.png" width={70} height={64} alt="LogoLuma"></Image>
-        <h2>ხშირად დასმული კითხვები</h2>
+    <div className='flex justify-center flex-col'>
+      <nav className='flex justify-between items-center'>
+        <Image src='/luma.png' width={70} height={64} alt='LogoLuma'></Image>
+        {/* <h2>ხშირად დასმული კითხვები</h2> */}
+        <LangTransitionH2 title={'header.FAQ'} className='' />
         {/* <Image src="file.svg" width={40} height={64} alt="LogoLuma"></Image> */}
-        <h2>GEO</h2>
+        {/* <h2>GEO</h2> */}
+        <LocaleSwitcher />
       </nav>
-      <h1 className="text-[32px] font-bold text-center mt-[24px]">
+      <h1 className='text-[32px] font-bold text-center mt-[24px]'>
         დონორის სარეგისტრაციო ანკეტა
       </h1>
-      <div className="text-center border-[1px] rounded-4xl pl-8 pr-8 pt-2 pb-2 text-[15px] mt-[24px] m-auto bg-gray-200 font-bold">
+      <div className='text-center border-[1px] rounded-4xl pl-8 pr-8 pt-2 pb-2 text-[15px] mt-[24px] m-auto bg-gray-200 font-bold'>
         თქვენი მონაცემები დარჩება კონფიდენციალური
       </div>
-      <div className="w-full bg-black h-[1px] mt-[24px]"></div>
+      <div className='w-full bg-black h-[1px] mt-[24px]'></div>
     </div>
   );
 }
